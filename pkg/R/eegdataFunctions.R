28324f6480c68183d2c6f473a93f059036883a28
wdweeda (Wouter Weeda) <w.d.weeda@fsw.leidenuniv.nl>
2011-04-28 13:09:19 +0000

    peakModel updates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/swale@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/eegdataFunctions.R
+++ pkg/R/eegdataFunctions.R
@@ -35,8 +35,8 @@ function(eegdat,window)
 }
 
 peakModel <-
-		function(data,window=NULL,thres=1e-03,plot=F) 
-#peakPick using 
+function(data,window=NULL,plot=F,simdat=NULL,simeeg=NULL) 
+#peakPick using derivative
 {
 	
 	trials = nrow(data)
@@ -46,7 +46,7 @@ peakModel <-
 	for(i in 1:trials) {
 		
 		deriv = fdDeriv(matrix(data[i,],,1))
-		peaks = which(abs(deriv)<thres)
+		peaks = zerocross(deriv)
 		
 		if(!is.null(window)) {
 			rm = c(which(peaks<window[1]),which(peaks>window[2]))
@@ -55,10 +55,24 @@ peakModel <-
 		
 		if(plot) {
 			par(ask=T)
-			plot(data[i,],type='l',lwd=2,col=1,bty='n',main='peaks')
-			points(peaks,data[i,peaks],pch=19,col=2)	
+			
+			if(!is.null(simeeg)) {
+				plot(simeeg[i,],col='gray',type='l',lty=1,bty='n',main='peaks')
+				lines(data[i,],lwd=2,col=1)
+				rsq = (sum(simeeg[i,]^2)-sum((simeeg[i,]-data[i,])^2))/sum(simeeg[i,]^2)
+				text(200,5,round(rsq,6))
+				
+			} else plot(data[i,],type='l',lwd=2,col=1,bty='n',main='peaks')
+			
+			points(peaks,data[i,peaks],pch=19,col=2)
+			points(window,data[i,window],pch=19,col=4)
+			
+			if(!is.null(simdat)) {
+				points(150-simdat$lats[i,1],data[i,round(150-simdat$lats[i,1])],col=3,pch=21,cex=1.5)
+				
+			}
 		}
-		
+	
 		peakvec[[i]] = list(lat=peaks,amps=data[i,peaks])
 		
 	}
@@ -66,5 +80,22 @@ peakModel <-
 	return(peakvec)	
 }
 
+
+zerocross <- function(vec) 
+{
+	len = length(vec)
+	sig = sign(vec)
+	zc = numeric(0)
+	
+	for(i in 1:(len-1)) {
+		
+		if(sig[i]==1 & sig[i+1]==-1) zc = c(zc,(i-1+which.min(abs(c(sig[i],sig[i+1])))))
+		if(sig[i]==-1 & sig[i+1]==1) zc = c(zc,(i-1+which.min(abs(c(sig[i],sig[i+1])))))
+	
+	}
+	
+	return(zc)
+	
+}
 
 
